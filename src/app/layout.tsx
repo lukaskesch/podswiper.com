037f58349bb9bec9069fe84cc00ca39566fbbdee
Lukas Kesch <lukas.kesch@gmail.com>
2025-02-22 12:47:02 +0100

    Update title tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <title>PodSwiper</title>
+        <title>PodSwiper | Your tool to select and save podcasts episodes</title>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
