88b7032077962af942878647545a586360e08b0e
Lukas Kesch <lukas.kesch@gmail.com>
2025-02-22 12:47:28 +0100

    Add meta description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <title>PodSwiper | Your tool to select and save podcasts episodes</title>
+        <meta name="description" content="PodSwiper is your tool to select and save podcasts episodes you want to listen to" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
